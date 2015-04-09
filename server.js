3fc4ec047cb6a5e6ee770241dd9e6969fbd62213
Adam Back <adb33@email.arizona.edu>
2015-04-09 15:10:26 -0700

    (cleanup) Removed unneeded passport line.

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var app            = express();
 var bodyParser     = require( 'body-parser' );
 var port           = process.env.PORT || 3000;
 
-app.use( passport.initialize() );
-
 var allowCrossDomain = function( req, res, next ) {
     res.header( 'Access-Control-Allow-Origin', '*' );
     res.header( 'Access-Control-Allow-Methods', 'GET,POST' );
