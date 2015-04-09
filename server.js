baf96d1eb803379c3741482119d9a2ddccac5b06
Adam Back <adb33@email.arizona.edu>
2015-04-09 14:28:26 -0700

    (feat) Built out server file to include sophisticated routing for scalability.

---
+++ server.js
@@ -0,0 +1,29 @@
+var express        = require( 'express' );
+var app            = express();
+var bodyParser     = require( 'body-parser' );
+var port           = process.env.PORT || 9000;
+
+app.use( passport.initialize() );
+
+var allowCrossDomain = function( req, res, next ) {
+    res.header( 'Access-Control-Allow-Origin', '*' );
+    res.header( 'Access-Control-Allow-Methods', 'GET,POST' );
+    res.header( 'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With' );
+    next();
+}
+;
+app.use( bodyParser.json() );
+app.use( allowCrossDomain );
+app.use( express.static( __dirname + '/public' ) );
+
+var EKMRouter = express.Router();
+
+var routers = {};
+
+routers.EKMRouter = EKMRouter;
+
+require( './config.js' )( app, express, routers );
+
+app.listen(port);
+console.log( 'Volta server running on port:',  port );
+exports = module.exports = app;
