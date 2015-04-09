f492e26ce71c749c8245820efefee4ef1f38e551
Adam Back <adb33@email.arizona.edu>
2015-04-09 16:01:16 -0700

    (feat) Build middleware handling.

---
+++ middleware.js
@@ -0,0 +1,31 @@
+/*
+ * MiddleWare for the entire app
+*/
+
+module.exports = exports = {
+  logError: function ( err, req, res, next ) {
+    if ( err ) {
+      console.error( err );
+      return next e rr );
+    }
+    next();
+  },
+
+  handleError: function ( err, req, res, next ) {
+    if ( err ) {
+      res.send( err, 500  );
+    }
+  },
+
+  cors: function ( req, res, next ) {
+    res.header( 'Access-Control-Allow-Origin', '*' );
+    res.header( 'Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS' );
+    res.header( 'Access-Control-Allow-Headers', 'Content-type, Authorization' );
+
+    if ( req.method === 'Options' ) {
+      res.send( 200 );
+    } else {
+      return next();
+    }
+  }
+};
