59760f10d85799aa451df9dab8c9b6abf798e62c
Adam Back <adb33@email.arizona.edu>
2015-04-09 17:15:04 -0700

    EKM route serving fake data.

--- routes/ekmRoutes.js
+++ routes/ekmRoutes.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Made it to root path: EKM');
-});
+// for ekm/
+
+router.route( '/' )
+  .get(function( req, res ) {
+    res.send( 'You\'ve reached ' + req.url + '.' );
+  });
+
+router.route( '/:id' )
+  .get(function( req, res ) {
+    res.send( 'You\'ve reached ' + req.url + '.' )
+  });
 
 module.exports = router;
-
-  // router.route( '/' )
-  //   .get(function( req, res ) {
-      // res.send( 'You\'ve reached ' + req.url + '.' );
-
-  // router.route( '/:id' )
-  //   .get(function( req, res ) {
-  //     res.send( 'You\'ve reached ' + req.url + '.' )
-  //   });
