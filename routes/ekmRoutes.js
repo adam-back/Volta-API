c53aa2ea3c545957385a9c2aae3e3ee06c5bbab2
Adam Back <adb33@email.arizona.edu>
2015-04-10 14:03:25 -0700

    (doc) Wrote pseudocode for queue system.

--- routes/ekmRoutes.js
+++ routes/ekmRoutes.js
@@ -17,9 +17,19 @@ router.route( '/' )
 
 router.route( '/:id' )
   .get(function( req, res ) {
+    // get data
     request( 'http://summary.ekmpush.com/summary?meters=' + req.url.substring(1) + '&key=' + APIkey + '&report=dy&format=json&offset=0&limit=1', function( error, response, body ) {
+      // serve it
       res.send( body );
+      // write it to database
     });
+
+    // TODO:
+    // check the database for a matching entry
+      // if it returns
+        // serve it
+      // if it isn't downloaded yet
+        // put it in the queue to download
   });
 
 module.exports = router;
