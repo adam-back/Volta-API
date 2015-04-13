fe662ae29f85fee33855ed14af598d5474defaa3
Adam Back <adb33@email.arizona.edu>
2015-04-13 15:46:06 -0700

    (cleanup) Removed www into server.js.

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var ekmRoutes = require('./routes/ekmRoutes');
+var http = require( 'http' );
+var sequelize = require( 'sequelize' );
+var models = require( './models');
 
 var app = express();
 
+// Configuration
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Route handling
+var ekmRoutes = require('./routes/ekmRoutes');
 
 app.use('/ekm', ekmRoutes);
 
@@ -21,7 +26,30 @@ app.get('*', function(req, res){
   res.send('I\'m afraid I can\'t do that, Hal', 404);
 });
 
-module.exports = app;
+////////////////////////////
+// Server startup
+////////////////////////////
 
-child_process.exec('./bin/www');
+var port = process.env.PORT || 3000;
+app.set( 'port', port );
+
+var server = http.createServer( app );
+
+// Create listeners
+server.on( 'error', function() {
+  console.error( 'Server error.' );
+});
+
+server.on('listening', function() {
+  console.log( 'Server listening on port:', port );
+});
+
+// Syncronize the models
+models.sequelize.sync()
+  // when sucessful
+  .then(function() {
+    // start to listen
+  });
+
+server.listen( port );
  
