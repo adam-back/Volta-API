3837046bb3f7b6297b1c48b61901264de28eba74
Adam Back <adb33@email.arizona.edu>
2015-04-13 14:22:18 -0700

    (cleanup) Took out a lot of the handling fluff in server and www.

--- server.js
+++ server.js
@@ -17,37 +17,9 @@ app.use(cookieParser());
 
 app.use('/ekm', ekmRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('*', function(req, res){
+  res.send('I\'m afraid I can\'t do that, Hal', 404);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
 
