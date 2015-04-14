a2e9fdd95bc69f57ad6ea03b93c1ecf742594c75
Adam Back <adb33@email.arizona.edu>
2015-04-13 17:53:40 -0700

    (cleanup) Replaced double quotes with single quotes.

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 var config = {
-  "development": {
-    "username": "adamback",
-    "password": null,
-    "database": "volta_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "port": 5432
+  'development': {
+    'username': 'adamback',
+    'password': null,
+    'database': 'volta_development',
+    'host': '127.0.0.1',
+    'dialect': 'postgres',
+    'port': 5432
   },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
-    "port": 5432
+  'production': {
+    'username': process.env.DB_USERNAME,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
+    'dialect': 'postgres',
+    'port': 5432
   }
 };
 
