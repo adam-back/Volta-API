b1e585473ab4a876130613df88cf54ace31eca74
Adam Back <adb33@email.arizona.edu>
2015-04-14 17:32:14 -0700

    (feat) Added more data fields to the Station model.

--- models/Station.js
+++ models/Station.js
@@ -1,23 +1,25 @@
 module.exports = function( sequelize, DataTypes ) {
   var Station = sequelize.define('Station', {
     id: {
-          type: Sequelize.INTEGER,
+          type: DataTypes.INTEGER,
           autoIncrement: true,
           primaryKey: true
       }, 
-    kin: Sequelize.STRING
-    ekmMeterSerialNumber: Sequelize.STRING,
-    location: Sequelize.STRING,
-    locationAddress: Sequelize.STRING,
-    locationGPS: Sequelize.Array(Sequelize.DECIMAL),
-    chargeType: Sequelize.INTEGER,
-    maxVolts: Sequelize.INTEGER,
-    maxAmps: Sequelize.INTEGER,
-    cumulativeKW: Sequelize.DECIMAL,
-    status: Sequelize.STRING,
-    inUse: Sequelize.BOOLEAN
-  };
+    kin: DataTypes.STRING,
+    siteNumber: DataTypes.INTEGER,
+    ekmPushMAC: DataTypes.STRING,
+    ekmOmnimeterSerial: DataTypes.STRING,
+    SIMCard: DataTypes.INTEGER,
+    location: DataTypes.STRING,
+    locationAddress: DataTypes.STRING,
+    locationGPS: DataTypes.ARRAY(DataTypes.DECIMAL),
+    chargeType: DataTypes.INTEGER,
+    maxVolts: DataTypes.INTEGER,
+    maxAmps: DataTypes.INTEGER,
+    cumulativeKW: DataTypes.DECIMAL,
+    status: DataTypes.STRING,
+    inUse: DataTypes.BOOLEAN
+  });
 
   return Station;
-});
-  
+};
