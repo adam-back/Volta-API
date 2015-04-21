acaf771742507c47c3b5f6856965cc3b3357c927
Adam Back <adb33@email.arizona.edu>
2015-04-21 14:05:07 -0700

    (feat) Updated index with full relationships and comments.

--- models/index.js
+++ models/index.js
@@ -8,10 +8,9 @@ var config    = require( '../config/config' )[ env ];
 var sequelize = new Sequelize( config.database, config.username, config.password, config );
 
 
-// describe relationships
-var createRelationshipsBetweenTables = function( m ) {
-  m.Station.hasMany( m.EKMreading, { as: { singular: 'reading', plural: 'readings'} } );
-};
+//////////////////
+// Import Schemas
+//////////////////
 
 fs.readdirSync( __dirname )
   .filter( function( file ) {
@@ -28,9 +27,35 @@ Object.keys( db ).forEach( function( modelName ) {
   }
 });
 
-createRelationshipsBetweenTables( db );
+//////////////////
+// Create relationships
+//////////////////
 
-sequelize.sync();
+// One-to-many
+db.Station.hasMany( db.EKMreading, { as: { singular: 'EKMReading', plural: 'EKMReadings' } } );
+db.Station.hasMany( db.WeatherReading, { as: { singular: 'weatherReading', plural: 'weatherReadings' } } );
+db.Station.hasMany( db.StationRating, { as: { singular: 'stationRating', plural: 'stationRatings' } } );
+db.Station.hasMany( db.StationReport, { as: { singular: 'stationReport', plural: 'stationReports' } } );
+db.Station.hasMany( db.ChargeHistory, { as: { singular: 'chargeHistory', plural: 'chargeHistories' } } );
+db.User.hasMany( db.StationRating, { as: { singular: 'stationRating', plural: 'stationRatings' } } );
+db.User.hasMany( db.StationReport, { as: { singular: 'stationReport', plural: 'stationReports' } } );
+db.User.hasMany( db.ChargeHistory, { as: { singular: 'chargeHistory', plural: 'chargeHistories' } } );
+
+// Many-to-many
+db.Car.belongsToMany( db.User, { through: 'UserCar' } );
+db.User.belongsToMany( db.Car, { through: 'UserCar' } );
+
+//////////////////
+// Sync
+//////////////////
+
+sequelize.sync()
+  .then(function() {
+    console.log( 'Successfully synced database on startup.' );
+  })
+  .catch(function( error ) {
+    console.log( 'Error syncing database on startup:', error );
+  });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
