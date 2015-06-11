f1dbcbd217e71200c7534c68e7f7a4be2f42f47a
Andrew Teich <andrewteich@me.com>
2015-06-11 16:56:49 -0700

    (refactor) Added Add/Update/Delete to Stations and Plugs

--- controllers/plug.controller.js
+++ controllers/plug.controller.js
@@ -1,5 +1,6 @@
 var request = require( 'request' );
 var plug = require( '../models').plug;
+var station = require( '../models').station;
 var express = require( 'express' );
 
 module.exports = exports = {
@@ -27,7 +28,7 @@ module.exports = exports = {
       res.status( 500 ).send( error );
     });
   },
-  getOnePlug: function ( req, res ) {
+  getOnePlugFromStation: function ( req, res ) {
     // query database for a plug with a certain station_id
     plug.findOne( { where: { station_id: req.params.id } } )
     .then(function( plugs ) {
@@ -36,6 +37,106 @@ module.exports = exports = {
         res.status( 404 ).send( '<p><strong>404</strong></p><p>A plug associated with that station id was not found.</p>' );
       } else {
         res.json( plugs );
+      }
+    })
+    .catch(function( error ) {
+      res.status( 500 ).send( error );
+    });
+  },
+  getOnePlug: function ( req, res ) {
+    // query database for a plug with a certain station_id
+    plug.find( { where: { id: req.url.substring( 1 ) } } )
+    .then(function( plug ) {
+      if( plug ) {
+        res.json( plug );
+      } else {
+        res.status( 404 ).send( 'Plug with that id not found in database.' );
+      }
+    })
+    .catch(function( error ) {
+      res.status( 500 ).send( error );
+    });
+  },
+  addPlug: function( req, res ) {
+    // Get the station so you can associate it
+    console.log('req body: ', req.body);
+    station.find( { where: { kin: req.body.kin } } )
+    .then(function( station ) {
+      console.log('found station');
+      // kin is not part of plug schema
+      delete req.body.kin;
+      // Validate that a plug with same omnimeter doesn't exist, create it
+      plug.findOrCreate( { where: { ekm_omnimeter_serial: req.body.ekm_omnimeter_serial }, defaults: req.body } )
+        .spread(function( plug, created ) {
+          console.log('create plug: ', plug, ' created: ', created);
+          if( created ) {
+            console.log('created plug successfully');
+            // associate
+            station.addPlug( plug )
+            // send boolean
+            res.json( { successfullyAddedPlug: created } );
+          } else {
+            // 409 = conflict
+            res.status( 409 ).send( plug );
+          }
+        });
+    })
+    .catch(function( error ) {
+      res.status( 500 ).send( error );
+    })
+  },
+  updatePlug: function(req, res) {
+    console.log(req.body);
+    // object looks like:
+    // { kin: #, changes: [ [ field, old, new ], [ field, old, new ] ] }
+    plug.find( { where: { ekm_omnimeter_serial: req.body.serialNumber, id: { $ne: req.body.id } } } )
+    .then(function( searchResult ) {
+      if( searchResult ) {
+        // throw error
+        throw searchResult;
+      // ok to add
+      } else {
+        return plug.find( { where: { id: req.body.id } } );
+      }
+    })
+    .then(function( plugToUpdate ) {
+      for ( var i = 0; i < req.body.changes.length; i++ ) {
+        var field = req.body.changes[ i ][ 0 ];
+        var newData = req.body.changes[ i ][ 2 ];
+        plugToUpdate[ field ] = newData;
+      }
+
+      return plugToUpdate.save();
+    })
+    .then(function( success ) {
+      res.status( 204 ).send();
+    })
+    .catch(function( error ) {
+      var text = {};
+      text.title = 'Duplicate Error';
+      text.message = 'There was already plug with the same omnimeter serial number.';
+      text.duplicateId = error.id;
+      res.status( 409 ).send( text );
+    });
+  },
+  deletePlug: function(req,res) {
+    // find the plug
+    plug.find( { where: { id: req.url.substring(1) } } )
+    .then(function( plug ) {
+      // if the plug exists
+      if( plug ) {
+        // delete it
+        return plug.destroy()
+      } else {
+      // else it doesn't exist
+        return 404;
+      }
+    })
+    .then(function( result ) {
+      if( result === 404 ) {
+        res.status( 404 ).send( 'A plug with that id could not be found' );
+      } else {
+        res.status( 204 ).send();
       }
     })
     .catch(function( error ) {
