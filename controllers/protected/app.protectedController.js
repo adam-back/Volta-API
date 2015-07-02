d588dadf5d1b6501ee941736d6157ace7a6c104c
Adam Back <adb33@email.arizona.edu>
2015-07-02 16:21:48 -0700

    (save) Grouping stations and plugs by common location.

--- controllers/protected/app.protectedController.js
+++ controllers/protected/app.protectedController.js
@@ -2,48 +2,81 @@ var station = require( '../../models').station;
 var station_report = require( '../../models' ).station_report;
 var express = require( 'express' );
 var async     = require( 'async' );
+var Q = require( 'q' );
+
+var connectStationsWithPlugs = function( stations ) {
+  var deferred = Q.defer();
+  var stationsAndPlugs = [];
+
+  // for each station
+  async.each( stations, function( station, cb ) {
+    // get only the values of the station
+    var plainStation = station.get( { plain: true } );
+    // get the associated plugs for the station
+    station.getPlugs()
+    .then(function( plugs ) {
+      // if there are plugs, i.e. push and cloudgate installed
+      if ( plugs && plugs.length > 0 ) {
+        // create a plugs field on the station
+        plainStation.plugs = [];
+        // for each plug on the station
+        for ( var i = 0; i < plugs.length; i++ ) {
+          // push the values of plug to the plugs array on station
+          plainStation.plugs[ plugs[ i ].number_on_station - 1 ] = plugs[ i ].get( { plain: true } );
+        }
+      // station not metered, no plugs
+      } else {
+        plainStation.plugs = null;
+      }
+
+      stationsAndPlugs.push( plainStation );
+      cb( null );
+    })
+    .catch(function( error ) {
+      cb( error );
+    });
+  }, function( error ) {
+    if ( error ) {
+      deferred.reject( error );
+    } else {
+      deferred.resolve( stationsAndPlugs );
+    }
+  });
+
+  return deferred.promise;
+};
+
+var groupByKin = function( stationsWithPlugs ) {
+}
 
 module.exports = exports = {
   getStationsAndPlugs: function ( req, res ) {
     var stationsAndPlugs = [];
+    var groupedByKin = {
+      // kin: common kin,
+      // location: coloquial location, eg. Serra Shopping Center,
+      // address: common location_address
+      // stations: array of stations
+        // [{
+            // id:
+            // kin:
+            // etc:
+            // plugs: array of plugs
+              // [{
+                  // id:
+                  // number_on_station:
+                  // etc
+              // }]
+        // }]
+    };
 
     // get all stations
     station.findAll()
     .then(function( stations ) {
-      // for each station
-      async.each( stations, function( station, cb ) {
-        // get only the values of the station
-        var plainStation = station.get( { plain: true } );
-        // get the associated plugs for the station
-        station.getPlugs()
-        .then(function( plugs ) {
-          // if there are plugs, i.e. push and cloudgate installed
-          if ( plugs && plugs.length > 0 ) {
-            // create a plugs field on the station
-            plainStation.plugs = [];
-            // for each plug on the station
-            for ( var i = 0; i < plugs.length; i++ ) {
-              // push the values of plug to the plugs array on station
-              plainStation.plugs[ plugs[ i ].number_on_station - 1 ] = plugs[ i ].get( { plain: true } );
-            }
-          // station not metered, no plugs
-          } else {
-            plainStation.plugs = null;
-          }
+      return connectStationsWithPlugs( stations );
+    })
+    .then(function( stationsAndPlugs ) {
 
-          stationsAndPlugs.push( plainStation );
-          cb( null );
-        })
-        .catch(function( error ) {
-          cb( error );
-        });
-      }, function( error ) {
-        if ( error ) {
-          res.status( 500 ).send( error );
-        } else {
-          res.json( stationsAndPlugs );
-        }
-      });
     })
     .catch(function( error ) {
       res.status( 500 ).send( error );
