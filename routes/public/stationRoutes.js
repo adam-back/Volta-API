c42fdc031bffd9d2019f40b90f4485f26abad136
Adam Back <adb33@email.arizona.edu>
2015-07-07 17:10:24 -0700

    (feat) Made summative data private on network route.

--- routes/public/stationRoutes.js
+++ routes/public/stationRoutes.js
@@ -6,14 +6,6 @@ var controller = require( '../../controllers/public/station.controller.js' );
 router.route( '/' )
   .get( controller.getAllStations );
 
-// for http://www.baseurl.com/stations/top10
-router.route( '/top10' )
-  .get( controller.getTopTenStations );
-
-// for http://www.baseurl.com/stations/cumulative
-router.route( '/cumulative' )
-  .get( controller.getCumulativeData );
-  
 // for http://www.baseurl.com/stations/001-0001-001-01-K
 // For Kill Switch, DO NOT TOUCH
 router.route( '/:kin' )
