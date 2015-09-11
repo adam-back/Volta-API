b541620f193cf2e5e2975efa2e1290f39088506d
Adam Back <adb33@email.arizona.edu>
2015-09-11 12:24:48 -0700

    (feat) Built routes for app sponsors.

--- controllers/protected/app.protectedController.js
+++ controllers/protected/app.protectedController.js
@@ -1,6 +1,7 @@
 var station = require( '../../models').station;
 var station_report = require( '../../models' ).station_report;
 var user = require( '../../models' ).user;
+var app_sponsor = require( '../../models' ).app_sponsor;
 var geocodeCache = require( '../../factories/geocodeCache.js' ).geocodeCache;
 var express = require( 'express' );
 var async     = require( 'async' );
@@ -269,6 +270,16 @@ module.exports = exports = {
     station_report.create( req.body )
     .then(function( success ) {
       res.status( 204 ).send(); // needs to be this for iOS app
+    })
+    .catch(function( error ) {
+      res.status( 500 ).send( error );
+    });
+  },
+  getAppSponsors: function ( req, res ) {
+    // app_sponsor.create( { company: 'Chevy', networks: [ 'SD', 'OC', 'LA', 'SB', 'NoCal' ], website_url: 'http://www.chevrolet.com/', twitter_url: 'https://twitter.com/chevyvolt', facebook_url: 'https://www.facebook.com/chevroletvolt/info?tab=page_info', instagram_url: 'https://instagram.com/chevrolet/?hl=en', logo_url: 'http://img2.wikia.nocookie.net/__cb20141116144915/logopedia/images/f/f4/Chevrolet_logo-2.png', station_query: { where: { network: { $in: [ 'SD', 'OC', 'LA', 'SB', 'NoCAl' ] } } }, banner_url: 'http://cdn.realestate.ph/ad3_320x50.jpg' } );
+    app_sponsor.findAll( { where: { current: true } } )
+    .then(function( sponsors ) {
+      res.send( sponsors );
     })
     .catch(function( error ) {
       res.status( 500 ).send( error );
