09bf2fe07d7eba94ce8ed22b8c8ef5ec82fff353
Adam Back <adb33@email.arizona.edu>
2015-12-18 13:39:51 -0800

    (feat) Created flat tree with station data.

---
+++ controllers/protected/d3.protectedController.js
@@ -0,0 +1,68 @@
+var express = require( 'express' );
+var Sequelize = require( 'sequelize' );
+var station = require( '../../models').station;
+var plug = require( '../../models').plug;
+var async     = require( 'async' );
+var Q = require( 'q' );
+var env = process.env.NODE_ENV || 'development';
+var config    = require( '../../config/config' )[ env ];
+
+module.exports = exports = {
+  getSunburstData: function ( req, res ) {
+    // get all the stations which we have data on
+    station.findAll( { attributes: [ 'kin', 'cumulative_kwh', 'location_address', 'location', 'network' ] }, { where: { cumulative_kwh: { $ne: null }  } } )
+    .then(function( stations ) {
+      var unique = {
+        network: {},
+        city: {},
+        location: {},
+        kin: {}
+      };
+
+      // sort into unique values
+      for ( var i = 0; i < stations.length; i++ ) {
+        var oneStation = stations[ i ];
+        var splitAddress = oneStation.location_address.split( ', ' );
+        var city = splitAddress[ splitAddress.length - 2 ];
+        unique.network[ oneStation.network ] = true;
+        unique.city[ city ] = oneStation.network;
+        unique.location[ oneStation.location ] = city;
+        // add every kin with relational data
+        unique.kin[ oneStation.kin ] = [ oneStation.location, oneStation.cumulative_kwh ];
+      }
+
+      // Create flat, parent-child data
+      var nodes = [];
+      // Root
+      nodes.push( { name: 'Meter kWh', parent: null } );
+
+      // Level 1: Networks
+      for ( var network in unique.network ) {
+        nodes.push( { name: network, parent: 'Meter kWh' } );
+      }
+
+      // Level 2: Cities
+      for ( var city in unique.city ) {
+        // { name: city, parent: network }
+        // { name: San Francisco, parent: NoCal }
+        nodes.push( { name: city, parent: unique.city[ city ] } );
+      }
+
+      // Level 3: Locations
+      for ( var location in unique.location ) {
+        // { name: location, parent: city }
+        // { name: Stonestown, parent: San Francisco }
+        nodes.push( { name: location, parent: unique.location[ location ] } );
+      }
+
+      // Level 4: Stations
+      for ( var kin in unique.kin ) {
+        // { name: kin, parent: location, size: cumulative_kwh }
+        // { name: 001-0001-001-01-K, parent: Stonestown, size: 1023.3 }
+        nodes.push( { name: kin, parent: unique.kin[ kin ][ 0 ], size: parseFloat( unique.kin[ kin ][ 1 ] ) } );
+      }
+
+      res.send( nodes );
+    });
+  }
+};
