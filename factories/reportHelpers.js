39474f58999318fddb51932c2250b4fdfeb4b871
Adam Back <adb33@email.arizona.edu>
2016-01-13 22:49:19 -0800

    (feat) Wrote monster report for per-station performance in the last 30 days.

--- factories/reportHelpers.js
+++ factories/reportHelpers.js
@@ -7,6 +7,18 @@ var charge_event = require( '../models').charge_event;
 var async     = require( 'async' );
 var ekm = require( './ekmFactory.js' );
 
+exports.orderByKin = function( collection ) {
+  collection.sort(function( a, b ) {
+    if ( a.kin.toLowerCase() < b.kin.toLowerCase() ) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  return collection;
+};
+
 exports.findMedian = function( collection ) {
   collection.sort(function( a, b ) {
     return a - b;
@@ -45,29 +57,41 @@ exports.countChargesAndDuration = function( chargeEvents ) {
   // save the total charge events
   data.totalChargeEvents = chargeEvents.length;
   // save the first day
-  data.firstChargeEvent = moment( new Date( chargeEvents[ 0 ].time_start ) ).format( 'MMM D, YYYY' );
+  data.firstChargeEvent = moment( chargeEvents[ 0 ].time_start ).format( 'MMM D, YYYY' );
 
   ////////////
   // COUNT //
+  var currentDay = moment( chargeEvents[ 0 ].time_start );
   // number of days
-  // number of charge events for each day
-  // total duration of usage
-  // durations for each
-  var currentDay = moment( new Date( chargeEvents[ 0 ].time_start ) );
   var numberOfDays = 1;
   var chargeEventsForTheDay = 1;
+  // number of charge events for each day
   var chargeEventsByDay = [];
+  // total duration of usage
   var totalChargeDuration = 0;
+  // durations for each charge event
   var chargeDurations = [];
+  // total kwh of all charge events
+  var totalKwh = 0;
+  // kwh for each charge event
+  var chargeKwh = [];
+
+  // setup with first one
   var lengthOfCharge = exports.calculateChargeEventDuration( chargeEvents[ 0 ] );
   totalChargeDuration += lengthOfCharge;
+  totalKwh += chargeEvents[ 0 ].kwh;
+
   chargeDurations.push( lengthOfCharge );
+  chargeKwh.push( chargeEvents[ 0 ].kwh );
 
   for ( var i = 1; i < chargeEvents.length; i++ ) {
-    var eventTime = moment( new Date( chargeEvents[ i ].time_start ) );
+    var eventTime = moment( chargeEvents[ i ].time_start );
     lengthOfCharge = exports.calculateChargeEventDuration( chargeEvents[ i ] );
     totalChargeDuration += lengthOfCharge;
+    totalKwh += chargeEvents[ i ].kwh;
     chargeDurations.push( lengthOfCharge );
+    chargeKwh.push( chargeEvents[ i ].kwh );
+
     // if it's a new day
     if ( eventTime.isAfter( currentDay, 'day' ) ) {
       numberOfDays++;
@@ -83,6 +107,7 @@ exports.countChargesAndDuration = function( chargeEvents ) {
   }
   // save total days
   data.totalChargeEventDays = numberOfDays;
+  data.cumulativeKwh = Number( totalkWh.toFixed( 1 ) );
 
   // average number of events per day
   data.averageChargeEventsPerDay = Math.round( data.totalChargeEvents / data.totalChargeEventDays );
@@ -92,8 +117,12 @@ exports.countChargesAndDuration = function( chargeEvents ) {
   data.averageDurationOfEvent = Math.round( totalChargeDuration / chargeDurations.length );
   // median duration of charge event
   data.medianDurationOfEvent = exports.findMedian( chargeDurations );
+  // average kWh of charge event
+  data.averageKwhOfEvent = Math.round( totalKwh / chargeKwh.length );
+  // median kWh of charge event
+  data.medianKwhOfEvent = exports.findMedian( chargeKwh );
 
-  return true;
+  return data;
 };
 
 exports.getBrokenPlugs = function () {
@@ -146,7 +175,9 @@ exports.getBrokenPlugs = function () {
   return deferred.promise;
 };
 
-exports.chargeEventsOverTime = function( where ) {
+exports.chargeEventsOverTime = function( where, timePeriod ) {
+  var timeNum = timePeriod[ 0 ];
+  var timeUnit = timePeriod[ 1 ];
   var query = where || { where: {}, order: 'id', raw: true };
   query.order = 'id';
   query.raw = true;
@@ -161,7 +192,7 @@ exports.chargeEventsOverTime = function( where ) {
   return charge_event.findAll( query )
   .then(function( chargeEvents ) {
     // set the first 30-min period
-    var currentPeriod = moment( chargeEvents[ 0 ].time_start ).add( 30, 'minutes' );
+    var currentPeriod = moment( chargeEvents[ 0 ].time_start ).add( timeNum, timeUnit );
 
     for ( var i = 0; i < chargeEvents.length; i++ ) {
       var chargeEvent = chargeEvents[ i ];
@@ -177,7 +208,7 @@ exports.chargeEventsOverTime = function( where ) {
         periods.push( { time: currentPeriod, events: collector.totalEvents, kwh: collector.kwh } );
 
         // set new period
-        currentPeriod = moment( currentPeriod.add( 30, 'minutes' ) );
+        currentPeriod = moment( currentPeriod.add( timeNum, timeUnit ) );
       }
 
       // always add
@@ -186,8 +217,64 @@ exports.chargeEventsOverTime = function( where ) {
     }
 
     // don't forget the last period
-    currentPeriod = moment( currentPeriod.add( 30, 'minutes' ) );
+    currentPeriod = moment( currentPeriod.add( timeNum, timeUnit ) );
     periods.push( { time: currentPeriod, events: collector.totalEvents, kwh: collector.kwh.toFixed( 1 ) } );
     return periods;
+  });
+};
+
+exports.chargesOverLastThirtyDaysForOneStation = function( oneStation ) {
+  var thirtyDaysAgo = moment().startOf( 'day' ).subtract( 1, 'day' ).subtract( 1, 'month' );
+  var where = {
+    where: {
+      id: oneStation.id,
+      time_start: {
+        $gt: thirtyDaysAgo.toDate()
+      },
+      time_stop: {
+        $ne: null
+      }
+    },
+    raw: true,
+    order: [ 'time_start', 'ASC' ]
+  };
+
+  return charge_event.findAll( where )
+  .then(function( eventsForStation ) {
+    var averagesAndMedians = exports.countChargesAndDuration( eventsForStation );
+    var consumerNumbers = exports.convertKwhToConsumerEquivalents( averagesAndMedians.cumulativeKwh );
+    var consumerNumbersAverage = exports.convertKwhToConsumerEquivalents( averagesAndMedians.averageKwhOfEvent );
+    var consumerNumbersMedian = exports.convertKwhToConsumerEquivalents( averagesAndMedians.medianKwhOfEvent );
+
+    var dataForCSV = {
+      kin: oneStation.kin,
+      location: oneStation.location,
+      since: moment( eventsForStation[ 0 ].time_start ).format( 'MMM D, YYYY' ),
+      // cumulative kWh
+      kWh: averagesAndMedians.cumulativeKwh,
+      carbon: consumerNumbers.offset,
+      miles: consumerNumbers.miles,
+      trees: consumerNumbers.trees,
+      gallons: consumerNumbers.miles,
+      // charge events
+      numberOfCharges: averagesAndMedians.totalChargeEvents,
+      averageChargeEventsPerDay: averagesAndMedians.averageChargeEventsPerDay,
+      medianChargeEventsPerDay: averagesAndMedians.medianChargeEventsPerDay,
+      averageDurationOfEvent: averagesAndMedians.averageDurationOfEvent,
+      medianDurationOfEvent: averagesAndMedians.medianDurationOfEvent,
+      // Average kwh per event
+      averageKwhOfEvent: averagesAndMedians.averageKwhOfEvent,
+      averageCarbonPerEvent: consumerNumbersAverage.offset,
+      averageMilesPerEvent: consumerNumbersAverage.miles,
+      averageTreesPerEvent: consumerNumbersAverage.trees,
+      averageGallonsPerEvent: consumerNumbersAverage.miles,
+      // Median kwh per event
+      medianKwhOfEvent: averagesAndMedians.medianKwhOfEvent,
+      medianCarbonPerEvent: consumerNumbersMedian.offset,
+      medianMilesPerEvent: consumerNumbersMedian.miles,
+      medianTreesPerEvent: consumerNumbersMedian.trees,
+      medianGallonsPerEvent: consumerNumbersMedian.miles
+    };
+    return dataForCSV;
   });
 };
