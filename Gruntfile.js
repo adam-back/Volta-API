65fec77342cd420abe26d123081310063dc5c1df
Adam Back <adb33@email.arizona.edu>
2016-02-08 15:22:19 -0800

    (test) Added testing.

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jasmine_node: {
+      task_name: {
+        options: {
+          coverage: {},
+          forceExit: false,
+          showColors: true,
+          match: '.',
+          matchAll: false,
+          specFolders: [ 'test' ],
+          extensions: 'js',
+          specNameMatcher: 'spec',
+          captureExceptions: false,
+          junitreport: {
+            report: false,
+            savePath : './build/reports/jasmine/',
+            useDotNotation: true,
+            consolidate: true
+          }
+        },
+        src: [ '*.js', 'factories/*.js' ]
+      }
+    },
+    watch: {
+      files: [ '*.js', 'factories/*.js', 'test/**/*.spec.js' ],
+      tasks: 'test'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', 'watch');
+  grunt.registerTask('test', 'jasmine_node');
+};
