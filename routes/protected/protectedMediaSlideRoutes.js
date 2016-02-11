898f8e4b938e4bfa7dd4c7f710e0c4260790f7ad
Andrew Teich <andrewteich@me.com>
2016-02-11 13:55:54 -0800

    (cleanup) Removed extraneous code from Media Slides.

--- routes/protected/protectedMediaSlideRoutes.js
+++ routes/protected/protectedMediaSlideRoutes.js
@@ -2,21 +2,13 @@ var express = require( 'express' );
 var router = express.Router();
 var controller = require( '../../controllers/protected/mediaSlide.protectedController.js' );
 
-// for http://www.baseurl.com/protected/mediaSlide
+// Used by Station Manager
 router.route( '/' )
   .get( controller.getAllMediaSlides )
   .post( controller.addMediaSlide )
-  .patch( controller.updateMediaSlide )
 
-// router.route( '/kin/:kin')
-//   .get( controller.getMediaSlidesByKin )
-
+// Will be used by Station Manager in the future
 router.route( '/:id')
 	.delete( controller.deleteMediaSlide )
-
-// // for http://www.baseurl.com/plug/42
-// router.route( '/:id' )
-//   .get( controller.getOnePlug )
-//   .delete( controller.deletePlug );
 
 module.exports = router;
