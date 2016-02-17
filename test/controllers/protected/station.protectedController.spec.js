2f8315e4a28c94737230a34aa287128dd46cc635
Adam Back <adb33@email.arizona.edu>
2016-02-17 15:52:36 -0800

    (test) Wrote first jwt-based test.

--- test/controllers/protected/station.protectedController.spec.js
+++ test/controllers/protected/station.protectedController.spec.js
@@ -3,11 +3,13 @@ var app = require( '../../../server.js' ).app;
 supertest = supertest( app );
 var Q = require( 'q' );
 var station = require( '../../../models' ).station;
+var createToken = require( '../../jwtHelper' ).createToken;
 
 module.exports = function() {
   describe('stations/count', function() {
     describe('GET', function() {
       var countStations;
+      var token = createToken( 5 );
 
       beforeEach(function() {
         countStations = Q.defer();
@@ -15,26 +17,36 @@ module.exports = function() {
       });
 
       it('should be protected', function( done ) {
-        supertest.get( '/protected/stations/count' )
+        supertest.get( '/protected/station/count' )
         .expect( 401 )
         .end( done );
       });
 
-      xit('should return JSON of current station count', function( done ) {
+      it('should return JSON of current station count', function( done ) {
         var currentCount = 159;
         countStations.resolve( currentCount );
-        supertest.get( '/protected/stations/count' )
+        supertest.get( '/protected/station/count' )
+        .set( 'Authorization', 'Bearer ' + token )
         .expect( 200 )
         .expect( 'Content-Type', /json/ )
         .expect( JSON.stringify( currentCount ) )
+        .expect(function( res ) {
+          expect( station.count ).toHaveBeenCalled();
+          expect( station.count ).toHaveBeenCalledWith();
+        })
         .end( done );
       });
 
-      xit('should return 500 error for failure', function( done ) {
-        countStations.reject( 'Couldn\'t find all stations.' );
-        supertest.get( '/stations' )
+      it('should return 500 error for failure', function( done ) {
+        countStations.reject( 'Couldn\'t count all stations.' );
+        supertest.get( '/protected/station/count' )
+        .set( 'Authorization', 'Bearer ' + token )
         .expect( 500 )
-        .expect( 'Couldn\'t find all stations.' )
+        .expect( 'Couldn\'t count all stations.' )
+        .expect(function( res ) {
+          expect( station.count ).toHaveBeenCalled();
+          expect( station.count ).toHaveBeenCalledWith();
+        })
         .end( done );
       });
     });
