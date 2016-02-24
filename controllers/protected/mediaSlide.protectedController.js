b0fc03497adfe2e8547fdd35897a0ab9eb779301
Adam Back <adb33@email.arizona.edu>
2016-02-24 14:48:18 -0800

    (refactor) Added missing semicolons, notes, and error handling.

--- controllers/protected/mediaSlide.protectedController.js
+++ controllers/protected/mediaSlide.protectedController.js
@@ -1,10 +1,7 @@
-var request = require( 'request' );
 var mediaSlide = require( '../../models' ).media_slide;
-var express = require( 'express' );
-var async = require( 'async' );
 
 module.exports = exports = {
-  
+
   // Used by Station Manager
   getAllMediaSlides: function ( req, res ) {
     mediaSlide.findAll()
@@ -12,7 +9,6 @@ module.exports = exports = {
       res.json( Slides );
     })
     .catch(function( error ) {
-      console.log( 'promise blew up', error );
       res.status( 500 ).send( error );
     });
   },
@@ -24,18 +20,19 @@ module.exports = exports = {
     .spread(function( Slide, created ) {
       // send boolean
       res.json( Slide );
+    })
+    .catch(function( error ) {
+      res.status( 500 ).send( error );
     });
   },
 
   // Will be used by Station Manager in the future
   deleteMediaSlide: function ( req, res ) {
-    console.log( '\n\n DELETE SLIDE #', req.params.id );
     mediaSlide.destroy({ where: { id: req.params.id } } )
     .then( function( slide ) {
       res.json( slide );
     })
     .catch( function( error ) {
-      console.log( error );
       res.status( 500 ).send( error );
     });
   },
