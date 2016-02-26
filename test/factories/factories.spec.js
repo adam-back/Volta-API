aac17fa55637cd64e989268c7054797023a900ba
Adam Back <adb33@email.arizona.edu>
2016-02-26 11:41:57 -0800

    (cleanup) Removed extra semicolon.

--- test/factories/factories.spec.js
+++ test/factories/factories.spec.js
@@ -3,7 +3,7 @@ var csvFactoryTests = require( './csvFactory.spec.js' );
 var ekmFactoryTests = require( './ekmFactory.spec.js' );
 var reportHelpersTests = require( './reportHelpers.spec.js' );
 var eventsOverTimeTests = require( './reports/eventsOverTime.spec.js' );
-var kwhGrowthOverTimeTests = require( './reports/kwhGrowthOverTime.spec.js' );;
+var kwhGrowthOverTimeTests = require( './reports/kwhGrowthOverTime.spec.js' );
 
 describe('Factories', function() {
   describe('REPORT HELPERS', function() {
