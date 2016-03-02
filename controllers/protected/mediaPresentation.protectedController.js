3b80a0485fe465f79a4047a1e04d7758d41c1eb3
Adam Back <adb33@email.arizona.edu>
2016-03-02 12:24:01 -0800

    (test) addMediaPresentation.

--- controllers/protected/mediaPresentation.protectedController.js
+++ controllers/protected/mediaPresentation.protectedController.js
@@ -2,95 +2,90 @@ var mediaPresentation = require( '../../models').media_presentation;
 var mediaSchedule = require( '../../models' ).media_schedule;
 var station = require( '../../models').station;
 var async = require( 'async' );
+var Q = require( 'q' );
 
 module.exports = exports = {
-  getMediaPresentations: function( req, res, whereObject, keyToGet ) {
+  formatMediaSlidesForPresentations: function( presentations, keyToGet ) {
+    var deferred = Q.defer();
 
-    var receivedPresentations = function( presentations ) {
-      if( !presentations ) {
-        throw new Error( 'No presentations found' );
+    if( !presentations ) {
+      deferred.reject( 'No presentations found' );
+    }
+
+    //get slide ids
+    var calls = [];
+    var makeObjectIntoArray = function( obj ) {
+      var array = [];
+      for( var key in obj ) {
+        var contents = {};
+        contents.key = key;
+        contents.contents = obj[ key ];
+        array.push( contents );
       }
-
-      //get slide ids
-      var calls = [];
-      var makeObjectIntoArray = function( obj ) {
-        var array = [];
-        for( var key in obj ) {
-          var contents = {};
-          contents.key = key;
-          contents.contents = obj[ key ];
-          array.push( contents );
-        }
-        return array;
-      };
-
-      async.each( makeObjectIntoArray( presentations ), function( presentation, callback ) {
-        presentation.contents.getMediaSlides()
-        .then( function( slideData ) {
-          // console.log( 'slideData', slideData );
-
-          var slidesById = {};
-          // var slideIds = [];
-
-          for( var i=0; i<slideData.length; i++ ) {
-            // console.log('slideId: ', slideData[ i ].dataValues[ keyToGet ] );
-            var slideId = slideData[ i ].dataValues.id;
-            var slideValue = slideData[ i ].dataValues[ keyToGet ];
-            // slideIds.push( slideData[ i ].dataValues[ keyToGet ] );
-            slidesById[ slideId ] = slideValue;
-          }
-
-          // console.log( 'slidesById', slidesById );
-
-          //order the urls
-          var orderedURLs = [];
-          var slideOrder = presentation.contents.dataValues.slide_order;
-          for( var i=0; i<slideOrder.length; i++ ) {
-            var slideId = slideOrder[ i ];
-            if( !slidesById[ slideId ] ) {
-              console.log( 'MISSING SLIDE URL', slideId );
-              throw new Error( 'Slide ' + slideId + ' is missing URL' );
-            }
-
-            orderedURLs.push( slidesById[ slideId ] );
-          }
-
-          // presentation.contents.dataValues.slideIds = slideIds;
-          presentation.contents.dataValues.slideIds = orderedURLs;
-          callback( null );
-        })
-        .catch( function( error ) {
-          callback( error );
-        });
-      }, function( error ) {
-        if( error ) {
-          console.log( '\n\n', error, '\n\n' );
-          throw new Error( 'Slides are missing' );
-        } else {
-          res.json( presentations );
-        }
-      });
+      return array;
     };
 
-    if( whereObject ) {
-      mediaPresentation.findAll( whereObject )
-      .then( receivedPresentations )
+    // loop over presentations
+    async.each( makeObjectIntoArray( presentations ), function( presentation, callback ) {
+      // get media slides for each presentation
+      presentation.contents.getMediaSlides()
+      .then( function( slideData ) {
+        var slidesById = {};
+
+        // make id: value pair from each slide
+        for( var i=0; i<slideData.length; i++ ) {
+          var slideId = slideData[ i ].dataValues.id;
+          var slideValue = slideData[ i ].dataValues[ keyToGet ];
+          // slideIds.push( slideData[ i ].dataValues[ keyToGet ] );
+          slidesById[ slideId ] = slideValue;
+        }
+
+        //order the urls
+        var orderedURLs = [];
+        var slideOrder = presentation.contents.dataValues.slide_order;
+        for( var i=0; i<slideOrder.length; i++ ) {
+          var slideId = slideOrder[ i ];
+          if( !slidesById[ slideId ] ) {
+            throw new Error( 'Slide ' + slideId + ' is missing URL' );
+          }
+
+          orderedURLs.push( slidesById[ slideId ] );
+        }
+
+        // presentation.contents.dataValues.slideIds = slideIds;
+        presentation.contents.dataValues.slideIds = orderedURLs;
+        callback( null );
+      })
       .catch( function( error ) {
-        console.log( 'YOU BROKE YOUR PROMISE!', error );
-        res.status( 500 ).send( error );
+        callback( error );
       });
-    } else {
-      mediaPresentation.findAll()
-      .then( receivedPresentations )
-      .catch( function( error ) {
-        console.log( 'YOU BROKE YOUR PROMISE!', error );
-        res.status( 500 ).send( error );
-      });
-    }
+    }, function( error ) {
+      if( error ) {
+        deferred.reject( 'Slides are missing.' );
+      } else {
+        deferred.resolve( presentations );
+      }
+    });
+
+    return deferred.promise;
+  },
+  getMediaPresentations: function( whereObject, keyToGet ) {
+    var query = whereObject || {};
+
+    return mediaPresentation.findAll( query )
+    .then(function( mediaPresentations ) {
+      return exports.formatMediaSlidesForPresentations( mediaPresentations, keyToGet );
+    });
   },
   // Used by Station Manager
   getAllMediaPresentations: function ( req, res ) {
-    exports.getMediaPresentations( req, res, null, 'id' );
+    exports.getMediaPresentations( null, 'id' )
+    .then(function( formattedPresentations ) {
+      res.json( formattedPresentations );
+    })
+    .catch(function( error ) {
+      res.status( 500 ).send( error );
+    });
   },
 
   // Used by Station Manager
@@ -116,11 +111,21 @@ module.exports = exports = {
           mediaSlideIds.push( req.body.mediaSlides[ i ].id );
         }
 
-        presentation.addMediaSlides( mediaSlideIds );
+        return presentation.addMediaSlides( mediaSlideIds )
+        .then(function() {
+          return Q( created );
+        });
+      } else {
+        return Q( created );
       }
 
+    })
+    .then(function( wasCreated ) {
       // send boolean
-      res.json( { successfullyAddedmediaPresentation: created } );
+      res.json( { successfullyAddedmediaPresentation: wasCreated } );
+    })
+    .catch(function( error ) {
+      res.status( 500 ).send( error );
     });
   },
 
@@ -143,12 +148,13 @@ module.exports = exports = {
 
   // Used by Media Player
   getMediaPresentationById: function( req, res ) {
-    var id = req.params.id;
-    exports.getMediaPresentations( req, res, {
-      where: {
-        id: id
-      }
-    }, 'mediaUrl' );
+    exports.getMediaPresentations( { where: { id: req.params.id } }, 'mediaUrl' )
+    .then(function( oneMediaPresentation ) {
+      res.json( oneMediaPresentation );
+    })
+    .catch(function( error ) {
+      res.status( 500 ).send( error );
+    });
   },
 
   // getMediaPresentationsWithSlides
--- test/controllers/protected/apiTests/mediaPresentation.protectedController.spec.js
+++ test/controllers/protected/apiTests/mediaPresentation.protectedController.spec.js
@@ -6,19 +6,162 @@ var app = require( '../../../../server.js' ).app;
 supertest = supertest( app );
 var Q = require( 'q' );
 var createToken = require( '../../../jwtHelper' ).createToken;
+var controller = require( '../../../../controllers/protected/mediaPresentation.protectedController.js' );
 var token = createToken( 5 );
 
 module.exports = function() {
   describe('PRESENTATIONS', function() {
     describe('mediaPresentation/', function() {
+      var route = '/protected/mediaPresentation';
+
       describe('GET', function() {
+        var getPresentations;
+
+        beforeEach(function() {
+          getPresentations = Q.defer();
+          spyOn( controller, 'getMediaPresentations' ).andReturn( getPresentations.promise );
+        });
+
+        it('should be a defined route (not 404)', function( done ) {
+          getPresentations.reject();
+          supertest.get( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .expect(function( res ) {
+            expect( res.statusCode ).not.toBe( 404 );
+          })
+          .end( done );
+        });
+
+        it('should get all media presentations', function( done ) {
+          getPresentations.reject();
+          supertest.get( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .expect(function( res ) {
+            expect( controller.getMediaPresentations ).toHaveBeenCalled();
+            expect( controller.getMediaPresentations ).toHaveBeenCalledWith( null, 'id' );
+          })
+          .end( done );
+        });
+
+        it('should resolve 200 formatted presentations', function( done ) {
+          getPresentations.resolve( { key: true } );
+          supertest.get( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .expect( 200 )
+          .expect( 'Content-Type', /json/ )
+          .expect( { key: true } )
+          .end( done );
+        });
+
+        it('should return 500 failure for error', function( done ) {
+          getPresentations.reject( 'Test' );
+          supertest.get( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .expect( 500 )
+          .expect( 'Test' )
+          .expect(function( res ) {
+            expect( controller.getMediaPresentations ).toHaveBeenCalled();
+          })
+          .end( done );
+        });
       });
 
       describe('POST', function() {
+        var createPresentation, newPresentation, addSlides, body;
+
+        beforeEach(function() {
+          createPresentation = Q.defer();
+          addSlides = Q.defer();
+          newPresentation = models.media_presentation.build( { id: 1, name: 'Dog', active: false, slide_order: [ '1', '2' ] } );
+          spyOn( models.media_presentation, 'findOrCreate' ).andReturn( createPresentation.promise );
+          spyOn( newPresentation, 'addMediaSlides' ).andReturn( addSlides.promise );
+          body = {
+            name: 'Dog',
+            slideOrder: [ '1', '2' ],
+            mediaSlides: [ { id: 3 }, { id: 4 } ]
+          };
+        });
+
+        it('should be a defined route (not 404)', function( done ) {
+          createPresentation.reject();
+          supertest.post( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .send( body )
+          .expect(function( res ) {
+            expect( res.statusCode ).not.toBe( 404 );
+          })
+          .end( done );
+        });
+
+        it('should find or create presentation', function( done ) {
+          createPresentation.reject();
+          supertest.post( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .send( body )
+          .expect(function( res ) {
+            expect( models.media_presentation.findOrCreate ).toHaveBeenCalled();
+            expect( models.media_presentation.findOrCreate ).toHaveBeenCalledWith( { where: { name: 'Dog' }, defaults: { name: 'Dog', active: false, slide_order: [ '1', '2' ] } } );
+          })
+          .end( done );
+        });
+
+        it('should add media slides if presentation was created', function( done ) {
+          createPresentation.resolve( [ newPresentation, true ] );
+          addSlides.reject();
+          supertest.post( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .send( body )
+          .expect(function( res ) {
+            expect( newPresentation.addMediaSlides ).toHaveBeenCalled();
+            expect( newPresentation.addMediaSlides ).toHaveBeenCalledWith( [ 3, 4 ] );
+          })
+          .end( done );
+        });
+
+        it('should resolve 200 true if creation successful', function( done ) {
+          createPresentation.resolve( [ newPresentation, true ] );
+          addSlides.resolve();
+          supertest.post( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .send( body )
+          .expect( 200 )
+          .expect( 'Content-Type', /json/ )
+          .expect( { successfullyAddedmediaPresentation: true } )
+          .end( done );
+        });
+
+        it('should resolve 200 false if presentation was found', function( done ) {
+          createPresentation.resolve( [ newPresentation, false ] );
+          supertest.post( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .send( body )
+          .expect( 200 )
+          .expect( 'Content-Type', /json/ )
+          .expect( { successfullyAddedmediaPresentation: false } )
+          .expect(function( res ) {
+            expect( newPresentation.addMediaSlides ).not.toHaveBeenCalled();
+          })
+          .end( done );
+        });
+
+        it('should return 500 failure for error', function( done ) {
+          createPresentation.reject( 'Test' );
+          supertest.post( route )
+          .set( 'Authorization', 'Bearer ' + token )
+          .send( body )
+          .expect( 500 )
+          .expect( 'Test' )
+          .expect(function( res ) {
+            expect( models.media_presentation.findOrCreate ).toHaveBeenCalled();
+          })
+          .end( done );
+        });
       });
     });
 
     describe('mediaPresentation/:id', function() {
+      var route = '/protected/mediaPresentation/';
+
       describe('GET', function() {
       });
 
