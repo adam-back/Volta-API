8760d505f50e176e80ad0a913dc01d32d7e9fdf2
Adam Back <adb33@email.arizona.edu>
2016-03-03 11:47:02 -0800

    (feat) Gruntfile now watches subdirectories in factories/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      files: [ '*.js', 'controllers/**/*.js', 'factories/*.js', 'test/**/*.spec.js' ],
+      files: [ '*.js', 'controllers/**/*.js', 'factories/**/*.js', 'test/**/*.spec.js' ],
       tasks: 'test'
     }
   });
