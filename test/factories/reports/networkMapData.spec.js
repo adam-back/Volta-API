9239cb8bf08a0d97bd6476e976336f95eba7f85c
Adam Back <adb33@email.arizona.edu>
2016-05-31 16:32:11 -0400

    (test) CountNetworkChargeEvents tested.

--- test/factories/reports/networkMapData.spec.js
+++ test/factories/reports/networkMapData.spec.js
@@ -8,9 +8,60 @@ module.exports = function() {
   describe('networkMapData.js', function() {
     describe('countChargeEventsForNetwork', function() {
       var countChargeEventsForNetwork = factory.countChargeEventsForNetwork;
+      var countStations;
+
+      beforeEach(function() {
+        countStations = Q.defer();
+        spyOn( models.station, 'count' ).andReturn( countStations.promise );
+      });
 
       it('should be defined as a function', function() {
         expect( typeof countChargeEventsForNetwork ).toBe( 'function' );
+      });
+
+      it('should return a promise', function() {
+        var result = countChargeEventsForNetwork( 'LA' );
+        expect( Q.isPromise( result ) ).toBe( true );
+      });
+
+      it('should count charge events for a specific network', function( done ) {
+        countStations.reject( new Error( 'Test' ) );
+        countChargeEventsForNetwork( 'LA' )
+        .then(function( value ) {
+          expect( value ).not.toBeDefined();
+          done();
+        })
+        .catch(function( error ) {
+          expect( models.station.count ).toHaveBeenCalled();
+          expect( models.station.count ).toHaveBeenCalledWith( { where: { network: 'LA' }, include: [ { model: models.charge_event } ], raw: true } );
+          done();
+        });
+      });
+
+      it('should return a tuple of network name and count', function( done ) {
+        countStations.resolve( 1234 );
+        countChargeEventsForNetwork( 'LA' )
+        .then(function( value ) {
+          expect( value ).toEqual( [ 'LA', 1234 ] );
+          done();
+        })
+        .catch(function( error ) {
+          expect( error ).not.toBeDefined();
+          done();
+        });
+      });
+
+      it('should propagate an error', function( done ) {
+        countStations.reject( new Error( 'Test' ) );
+        countChargeEventsForNetwork( 'LA' )
+        .then(function( value ) {
+          expect( value ).not.toBeDefined();
+          done();
+        })
+        .catch(function( error ) {
+          expect( error.message ).toBe( 'Test' );
+          done();
+        });
       });
     });
 
