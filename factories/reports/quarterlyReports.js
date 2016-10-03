5dbfea64779395fa0cb1a053e8180f671b9e7522
Adam Back <adb33@email.arizona.edu>
2016-10-03 16:40:38 -0700

    (save) Added file to create quarterly report helper.

---
+++ factories/reports/quarterlyReports.js
@@ -0,0 +1,8 @@
+var models = require( '../../models');
+var csv    = require( '../csvFactory');
+var moment = require( 'moment' );
+var Q      = require( 'q' );
+
+exports.generateQuarterlyReport = function( quarter, year ) {
+
+};
